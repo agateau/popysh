f03abca09ccb5a1ce82cf6b397e121adb5e2f2d9
Aurélien Gâteau <mail@agateau.com>
2015-11-13 12:01:49 +0100

    Do not fail if a dir in $PATH does not exist

--- popysh/systemcommands.py
+++ popysh/systemcommands.py
@@ -4,6 +4,8 @@ from popysh.wrap import Wrap
 def _wrap_system_commands():
     dct = {}
     for dirs in os.environ['PATH'].split(os.pathsep):
+        if not os.path.isdir(dirs):
+            continue
         for name in os.listdir(dirs):
             cmd_name = name.replace('-', '_')
             dct[cmd_name] = Wrap(name)
