0bd6f37ab6935fe4fa212ae6f923f13debe93533
Aurélien Gâteau <mail@agateau.com>
2015-12-12 18:33:45 +0100

    Refactor to allow piping and a few other things

--- popysh/wrap.py
+++ popysh/wrap.py
@@ -1,21 +1,95 @@
 import subprocess
 
 
-class Output(object):
-    def __init__(self, proc, outf, errf):
-        self._proc = proc
-        self._outf = outf
-        self._errf = errf
-        self.returncode = proc.returncode
+class Process(object):
+    def __init__(self, args, input_data=None, merge_stderr=False):
+        self._args = args
+        self._input_data = input_data
 
-    def lines(self):
-        return self._outf.decode().splitlines()
+        if merge_stderr:
+            self._stderr = subprocess.STDOUT
+        else:
+            self._stderr = subprocess.PIPE
+
+        if self._input_data:
+            self._stdin = subprocess.PIPE
+        else:
+            self._stdin = None
+
+        self._pipe_head = None
+
+        self.returncode = -1
+
+    def run(self, stdout=None):
+        """
+        Run the process, sends stdout to the process stdout by default or to
+        the stdout argument if specified. `stdout` must be a file opened in
+        write or append mode.
+
+        Useful for long-time running commands.
+
+        Returns the process return code.
+        """
+        self._create_proc(stdout=stdout)
+        self._communicate()
+        return self.returncode
+
+    def as_bytes(self):
+        """
+        Run the process, returns its stdout as bytes
+        """
+        self._capture_output()
+        return self.out
+
+    def as_str(self):
+        """
+        Run the process, returns its stdout as a str
+        """
+        return self.as_bytes().decode()
+
+    def as_lines(self):
+        """
+        Run the process, returns its stdout as a list of str
+        """
+        return self.as_str().splitlines()
 
     def __repr__(self):
-        return self._outf.decode()
+        return self.as_str()
 
-    def bytes(self):
-        return self._outf
+    def pipe(self, next_process):
+        """
+        Sends stdout of the current process into stdin of `next_process`.
+        Returns a Process instance.
+
+        `next_process` must *not* have any `input_data`, since its input is going
+        to be the output of the current process.
+        """
+        assert next_process._input_data is None
+        self._create_proc(stdout=subprocess.PIPE)
+        if self._pipe_head is None:
+            self._pipe_head = self
+
+        next_process._stdin = self._proc.stdout
+        next_process._pipe_head = self._pipe_head
+        return next_process
+
+    def _capture_output(self):
+        self._create_proc(stdout=subprocess.PIPE)
+        return self._communicate()
+
+    def _create_proc(self, stdout=None):
+        self._proc = subprocess.Popen(self._args, stdin=self._stdin, stdout=stdout, stderr=self._stderr)
+
+    def _communicate(self):
+        if self._pipe_head:
+            self._pipe_head._write_stdin()
+        self.out, self.err = self._proc.communicate(input=self._input_data)
+        self.returncode = self._proc.returncode
+
+    def _write_stdin(self):
+        if self._input_data is not None:
+            self._proc.stdin.write(self._input_data)
+            self._proc.stdin.close()
 
 
 class Wrap(object):
@@ -26,22 +100,15 @@ class Wrap(object):
         """
         kwargs:
         - merge_stderr: set to True to mix stderr in stdout
-        - input: inptu to pipe in stdin (can be str or bytes)
+        - input: input to pipe in stdin (can be str or bytes)
         """
         cmd_args = [self.cmd]
 
         input_data = kwargs.get('input')
         if input_data:
-            stdin = subprocess.PIPE
             if isinstance(input_data, str):
                 input_data = input_data.encode()
-        else:
-            stdin = None
-        stdout = subprocess.PIPE
-        if kwargs.get('merge_stderr') == True:
-            stderr = subprocess.STDOUT
-        else:
-            stderr = subprocess.PIPE
+        merge_stderr = kwargs.get('merge_stderr', False)
 
         for arg in args:
             if type(arg) in (list, tuple):
@@ -49,7 +116,4 @@ class Wrap(object):
             else:
                 cmd_args.append(str(arg))
 
-        proc = subprocess.Popen(cmd_args, stdin=stdin, stdout=stdout,
-                                stderr=stderr)
-        outf, errf = proc.communicate(input=input_data)
-        return Output(proc, outf, errf)
+        return Process(cmd_args, input_data=input_data, merge_stderr=merge_stderr)
--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,31 @@
+from nose.tools import eq_
+
+
 def test_ls():
     from popysh.syscmd import ls
     ls()
 
-def test_pipe():
+def test_stdin_stdout():
     from popysh.syscmd import md5sum
     out = str(md5sum(input='bla'))
-    assert out == '128ecf542a35ac5270a87dc740918404  -\n'
+    eq_(out, '128ecf542a35ac5270a87dc740918404  -\n')
+
+def test_as_lines():
+    from popysh.syscmd import cat
+    lines = cat(input='bla\nbli').as_lines()
+    eq_(lines, ['bla', 'bli'])
+
+def test_returncode():
+    from popysh.syscmd import ls
+    proc = ls('/does-not-exist')
+    returncode = proc.run()
+    assert returncode != 0
+
+def test_pipe():
+    from popysh.syscmd import cat, grep, tr
+    out = cat(input='bla\nbli\nblo') \
+        .pipe(grep('bl[ai]')) \
+        .pipe(tr('b', 'c')) \
+        .as_bytes()
+
+    eq_(out, 'cla\ncli\n')
